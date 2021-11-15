f071762eb2be883a5bb21f0e40749d55c0f55d36
Sora Arakawa <sr@arkw.jp>
2021-11-15 11:38:22 +0900

    Add CKEditor4

--- index.php
+++ index.php
@@ -8,11 +8,17 @@ $data = file_get_contents("rewrite.txt");
 		<title>alexa-rewrite-web</title>
 		<meta charset="utf-8">
 		<meta name="robots" content="noindex, nofollow">
+		<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
 	</head>
 	<body>
+		<div id="data_temp" style="display: none;"><?php echo $data ?></div>
 		<form action="write.php" method="POST">
-			<textarea name="data"><?php echo $data ?></textarea><br>
+			<textarea name="data" id="data"></textarea><br>
 			<input type="submit" value="Submit">
 		</form>
+		<script type="text/javascript"><!--
+			CKEDITOR.replace("data");
+			CKEDITOR.instances.data.setData(document.getElementById("data_temp").innerHTML);
+		--></script>
 	</body>
 </html>
